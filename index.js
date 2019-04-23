06f6c8010cdde97869f88db8c112828ab07e6a56
funtz-web <adler.sun.funtz@gmail.com>
2019-04-23 23:21:08 +0800

    增加 head first html and css一书中资源

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
-first
-second
+window.onload = prepareLinks;
+function prepareLinks() {
+    var links = document.getElementsByTagName("a");
+    for (var i=0; i<links.length; i++) {
+        if(links[i].getAttribute("class") == "popup") {
+            links[i].onclick = function() {
+                popup(this.getAttribute("href"));
+                return false;
+            }
+        }
+    }
+}
