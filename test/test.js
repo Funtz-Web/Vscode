2b28f9eda1198af0320e51b46ef62260c1d4a1de
funtz-web <adler.sun.funtz@gmail.com>
2019-07-12 23:16:44 +0800

    ..

---
+++ test/test.js
@@ -0,0 +1,11 @@
+function findLongestWord(str) {
+    // 请把你的代码写在这里
+
+    console.log(str.split(" "));
+
+
+
+    return str.length;
+  }
+  
+  findLongestWord("The quick brown fox jumped over the lazy dog");
